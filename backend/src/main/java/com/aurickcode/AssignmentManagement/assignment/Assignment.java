207e174ac188031b71f9feccb18370b149a26c65
anurodhacharya <anurodh.acharya10@gmail.com>
2024-02-24 18:59:35 -0800

    User and Assignment Entity

---
+++ backend/src/main/java/com/aurickcode/AssignmentManagement/assignment/Assignment.java
@@ -0,0 +1,73 @@
+package com.aurickcode.AssignmentManagement.assignment;
+
+import com.aurickcode.AssignmentManagement.domain.User;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Assignment {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String status;
+
+    @Column
+    private String githubUrl;
+
+    @Column
+    private String branch;
+
+    @Column
+    private String codeReviewVideoUrl;
+
+    @ManyToOne(optional = false)
+    private User user;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getGithubUrl() {
+        return githubUrl;
+    }
+
+    public void setGithubUrl(String githubUrl) {
+        this.githubUrl = githubUrl;
+    }
+
+    public String getBranch() {
+        return branch;
+    }
+
+    public void setBranch(String branch) {
+        this.branch = branch;
+    }
+
+    public String getCodeReviewVideoUrl() {
+        return codeReviewVideoUrl;
+    }
+
+    public void setCodeReviewVideoUrl(String codeReviewVideoUrl) {
+        this.codeReviewVideoUrl = codeReviewVideoUrl;
+    }
+
+    
+}
