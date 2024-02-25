7404b36d7265e1d537caee99111500259beae25d
anurodhacharya <anurodh.acharya10@gmail.com>
2024-02-24 17:15:07 -0800

    Spring Boot Starter

---
+++ backend/src/test/java/com/aurickcode/AssignmentManagement/AssignmentManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aurickcode.AssignmentManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
