80e17176e306de693c68c998cb69ca281c97cba0
anurodhacharya <anurodh.acharya10@gmail.com>
2024-02-25 16:11:17 -0800

    JWT Authentication

--- backend/src/main/java/com/aurickcode/AssignmentManagement/security/SecurityFilterChainConfig.java
+++ backend/src/main/java/com/aurickcode/AssignmentManagement/security/SecurityFilterChainConfig.java
@@ -6,12 +6,26 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.ExceptionTranslationFilter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.aurickcode.AssignmentManagement.jwt.JWTAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityFilterChainConfig {
+
+    private final AuthenticationEntryPoint authenticationEntryPoint;
+    private final JWTAuthenticationFilter jwtAuthenticationFilter;
+
+    public SecurityFilterChainConfig(AuthenticationEntryPoint authenticationEntryPoint,
+                                    JWTAuthenticationFilter jwtAuthenticationFilter
+    ) {
+        this.authenticationEntryPoint = authenticationEntryPoint;
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -25,7 +39,12 @@ public class SecurityFilterChainConfig {
                         )
             .permitAll()
             .anyRequest()
-            .authenticated();
+            .authenticated()
+            .and()
+            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+            // .addFilterAfter(jwtAuthenticationFilter, ExceptionTranslationFilter.class)
+            .exceptionHandling()
+            .authenticationEntryPoint(authenticationEntryPoint);
 
         return http.build();
     }
--- backend/src/main/java/com/aurickcode/AssignmentManagement/user/User.java
+++ backend/src/main/java/com/aurickcode/AssignmentManagement/user/User.java
@@ -1,12 +1,6 @@
 package com.aurickcode.AssignmentManagement.user;
 
 import java.time.LocalDate;
-import java.util.Collection;
-import java.util.List;
-
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
