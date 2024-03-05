3d4758663143729f307965edf3280514ddf9cb43
anurodhacharya <anurodh.acharya10@gmail.com>
2024-03-05 13:26:48 -0800

    Fetch Functionality Added, Cors Issue Resolved

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,27 @@
 import './App.css';
 
-function App() {
+const App = () => {
+  const requestBody = {
+    "username": "Anurodh",
+    "password": "anurodh123"
+  }
+
+  const promise = fetch("http://localhost:8080/api/auth/login", {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "post",
+    body: JSON.stringify(requestBody)
+  });
+
+  promise.then((res) => {
+    console.log(res.headers.get('authorization'));
+
+    res.json().then((data) => {
+      console.log(data);
+    })
+  })
+
   return (
     <div className="App">
       <h1>Hello World!</h1>
